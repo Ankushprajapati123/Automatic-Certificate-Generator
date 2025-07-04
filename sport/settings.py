3f9e534e8287b58007ec4f11b9755219030066bd
Ankush Prajapati <168620853+Ankushprajapati123@users.noreply.github.com>
2025-07-04 20:23:01 +0530

    Update settings.py

--- sport/settings.py
+++ sport/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'sport.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'static', 'templets')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
